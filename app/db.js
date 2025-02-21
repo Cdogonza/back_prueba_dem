6764876d2d3b452164c061d9350179b31cdc1051
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2025-02-21 16:43:26 -0300

    connection alivee

--- app/db.js
+++ app/db.js
@@ -4,35 +4,26 @@ const dotenv = require('dotenv');
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-// Crear la conexión a la base de datos
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+// Crear el pool de conexiones
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-// Conectar a la base de datos
-db.connect(err => {
-    if (err) {
-        console.error('Error conectando a la base de datos:', err);
-        return;
-    }
-    console.log('Conectado a la base de datos MySQL');
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // Límite de conexiones simultáneas
+    queueLimit: 0 // No limitar la cantidad de consultas en cola
 });
 
+// Mantener la conexión activa
 setInterval(() => {
-    pool.query('SELECT 1', (err, results) => {
+    db.query('SELECT 1', (err, results) => {
         if (err) {
             console.error('Error manteniendo la conexión:', err);
         }
         // Aquí no necesitas hacer nada con los resultados
     });
 }, 5000); // Cada 5 segundos
-// Exportar la conexión para usarla en otros archivos
+
+// Exportar el pool para usarlo en otros archivos
 module.exports = db;
