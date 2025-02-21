e8412f0ff92d2159d748e617bce8720ab51b89e5
Cdogonza <gonzalo.paz@correo.ucu.edu.uy>
2025-02-21 16:40:33 -0300

    connection alive

--- app/db.js
+++ app/db.js
@@ -5,13 +5,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Crear la conexión a la base de datos
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
-
 // Conectar a la base de datos
 db.connect(err => {
     if (err) {
@@ -21,5 +26,13 @@ db.connect(err => {
     console.log('Conectado a la base de datos MySQL');
 });
 
+setInterval(() => {
+    pool.query('SELECT 1', (err, results) => {
+        if (err) {
+            console.error('Error manteniendo la conexión:', err);
+        }
+        // Aquí no necesitas hacer nada con los resultados
+    });
+}, 5000); // Cada 5 segundos
 // Exportar la conexión para usarla en otros archivos
 module.exports = db;
